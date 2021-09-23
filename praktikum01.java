e87cbf950145352baa2ebf6825b70a77de367adf
Alvinpt <90598958+Alvinpt@users.noreply.github.com>
2021-09-23 11:01:20 +0800

    Add files via upload

---
+++ praktikum01.java
@@ -0,0 +1,10 @@
+public class praktikum01{
+       public static void main(String[] args){
+  
+
+               // ini adalah fungsi pencetakan 
+                   System.out.println(" alvin sangat" + " baik");
+
+         
+         }
+ }
